15226df500c80730ff42ac5f2fefa4797960d28a
yangpeng <826168051@qq.com>
2018-10-26 22:48:44 +0800

    initial commit

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,40 @@
+package com.democome.smart;
+
+public class MergeTwoSortedLists {
+
+	// 21 Merge Two Sorted Lists
+	// 递归
+	public static void main(String[] args) {
+
+	}
+
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+		if (l1 == null) {
+			return l2;
+		}
+
+		if (l2 == null) {
+			return l1;
+		}
+		if (l1.val >= l2.val) {
+			ListNode node = l2;
+			node.next = mergeTwoLists(l1, l2.next);
+			return node;
+		} else {
+			ListNode node = l1;
+			node.next = mergeTwoLists(l1.next, l2);
+			return node;
+		}
+
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+}
---
+++ SingleNumber.java
@@ -0,0 +1,46 @@
+package com.democome.smart;
+
+public class SingleNumber {
+	// 136 Single Number
+
+	public static void main(String[] args) {
+
+	}
+
+	// 两个相同的数异或后为0；
+	// 0和一个数异或后为那个数；
+	// 异或运算满足交换律。
+
+	public static int singleNumber(int[] nums) {
+		int result = 0;
+		for (int i = 0; i < nums.length; i++) {
+			result ^= nums[i];
+		}
+		return result;
+	}
+
+	// public static int singleNumber(int[] nums) {
+	//
+	// if (nums.length == 1) {
+	// return nums[0];
+	// }
+	// Arrays.sort(nums);
+	//
+	// if (nums[0] != nums[1]) {
+	// return nums[0];
+	// }
+	//
+	// if (nums[nums.length - 2] != nums[nums.length - 1]) {
+	// return nums[nums.length - 1];
+	// }
+	//
+	// for (int i = 1; i < nums.length - 3; i++) {
+	// if (nums[i] != nums[i + 1] && nums[i + 1] != nums[i + 2]) {
+	// return nums[i + 1];
+	// }
+	// }
+	// return 0;
+	// }
+
+	// 参考https://blog.csdn.net/cloudox_/article/details/52459584
+}
