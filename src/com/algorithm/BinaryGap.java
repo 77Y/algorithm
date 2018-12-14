db1125946de009a2b9d54cce0190b448efa6eb42
yangpeng <826168051@qq.com>
2018-12-14 17:14:54 +0800

    BinaryGap

--- src/com/algorithm/BinaryGap.java
+++ src/com/algorithm/BinaryGap.java
@@ -5,10 +5,26 @@ public class BinaryGap {
 
 	public static void main(String[] args) {
 
+		System.err.println(binaryGap(5));
 	}
 
-	public int binaryGap(int N) {
+	public static int binaryGap(int N) {
 
-		return 0;
+		int[] arr = new int[32];
+		int count1 = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (((N >> i) & 1) == 1) {
+				arr[count1++] = i;
+			}
+		}
+
+		int distance = 0;
+
+		for (int i = 0; i < count1; i++) {
+			distance = Math.max(arr[i + 1] - arr[i], distance);
+		}
+
+		return distance;
 	}
 }
